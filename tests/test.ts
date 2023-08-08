f9b97467f64b9527b46550d2bb760eb0cb07ac08
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-08-08 11:15:44 +0200

    first commit

---
+++ tests/test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test';
+import { chromium } from '@playwright/test';
+
+test('index page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Welcome to Skeleton.' })).toBeVisible();
+});
