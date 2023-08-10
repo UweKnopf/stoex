d25700c5c432862f890460cc0b4a797a7961e152
leon-ma08 <leo.ma2608@gmail.com>
2023-08-11 00:42:35 +0200

    First test links of the feature showcase

---
+++ src/routes/dataInsights/+page.svelte
@@ -0,0 +1,7 @@
+<div class= "container justify-center mx-auto text-left p-24 md:text-center">
+    <div class="relative z-[1] space-y-6 pt-32 pb-24">
+        <div class="font-bold text-4xl tracking-tight">
+            Data Insights are our companys <span class="text-primary-500">stepping stool to alot of money</span>
+        </div>
+    </div>
+</div>
