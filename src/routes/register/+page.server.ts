db9c8dedfc3b3858aefdad1facaecc77c1c8fa58
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-08-12 00:08:35 +0200

    Basic login design and formvalidation on server

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,39 @@
+import { fail } from '@sveltejs/kit';
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms/server';
+
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword:z.string().min(6),
+}).superRefine(({ confirmPassword, password }, ctx) => {
+  if (confirmPassword !== password) {
+    ctx.addIssue({
+      code: "custom",
+      message: "The passwords did not match"
+    });
+  }
+});
+
+export const load = async () => {
+  // Server API:
+  const form = await superValidate(schema);
+
+  // Always return { form } in load and form actions.
+  return { form };
+};
+
+export const actions = {
+  default: async ({ request }) => {
+    const form = await superValidate(request, schema);
+    console.log('POST', form);
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    // TODO: Do something with the validated data
+
+    return { form };
+  }
+};
