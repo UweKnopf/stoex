89adbf84242e2d5856bd842a47774b36d19ebcd6
leon-ma08 <leo.ma2608@gmail.com>
2023-08-14 13:01:32 +0200

    First Test of Headline and Text

--- src/routes/dataInsights/+page.svelte
+++ src/routes/dataInsights/+page.svelte
@@ -1,7 +1,22 @@
-<div class= "container justify-center mx-auto text-left p-24 md:text-center">
+<!--Headline-->
+<div class= "container justify-start mx-1 text-left p-24 md:text-left">
     <div class="relative z-[1] space-y-6 pt-32 pb-24">
-        <div class="font-bold text-4xl tracking-tight">
-            Data Insights Test <span class="text-primary-500">test</span>
+        <div class="font-bold text-6xl tracking-tight">
+            <span class="text-primary-500">Data Insights Headline Test</span>
+        </div>
+        <div class= "font-normal text-4xl tracking-tight">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
         </div>
     </div>
+</div>
+<!--Informative Text--> 
+<div class= "container justify-start mx-1 text-left p-24 md:text-left">
+    <div class= "relative z-[1] space-y-6 pt-32 pb-24">
+        <div class= "font-bold text-6xl tracking-tight">
+            <span class= "text-primary-500">Data Insights Additional Content</span>
+        </div>
+        <div class= "font-normal text-4xl tracking-tight">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </div>
+    </div>    
 </div>
