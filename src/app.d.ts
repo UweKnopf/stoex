5ff3dd3d752dcb755d90743b6c41c9493e2be27f
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-08-28 16:01:23 +0200

    Basic Lucia Config

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,31 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+declare global {
+	namespace App {
+		// interface Locals {}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
+		interface Locals {
+			auth: import('lucia').AuthRequest;
+			validate: import("@lucia-auth/sveltekit").Validate
+			validateUser: import("@lucia-auth/sveltekit").ValidateUser
+			setSession: import("@lucia-auth/sveltekit").SetSession
+		}
+		
+	}
 }
+/// <reference types="lucia" />
+declare global {
+	namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth;
+		type DatabaseUserAttributes = {
+			username: string;
+		};
+		type DatabaseSessionAttributes = {};
+	}
+}
+
+// THIS IS IMPORTANT!!!
+export {};
---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,20 @@
+import { lucia } from "lucia";
+import { sveltekit } from "lucia/middleware";
+import { dev } from "$app/environment";
+import { prisma } from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export const auth = lucia({
+	env: dev ? "DEV" : "PROD",
+	middleware: sveltekit(),
+	adapter: prisma(client),
+    getUserAttributes: (data) => {
+		return {
+			username: data.username
+		};
+	}
+});
+
+export type Auth = typeof auth;
