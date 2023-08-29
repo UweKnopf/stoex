f1f188f395b6c3b0ef737a6028c942daa9a00f76
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-08-29 20:15:42 +0200

    Fix sol Input fields

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -13,7 +13,7 @@
 
 <SuperDebug data={$form} />
 
-<div class="container justify-center mx-auto text-left p-24 md:text-center max-w-xl">
+<div class="container justify-center mx-auto p-24 text-center max-w-xl">
     <div class="card p-6 space-y-6 shadow-xl">
         <p class="font-semibold">Welcome, login with</p>
         <div class="flex flex-wrap space-y-4 space-x-0 md:flex-nowrap md:space-x-4 md:space-y-0">
@@ -27,24 +27,24 @@
         
         <form class="space-y-4" method="POST">
             <label for="email" class="label">
-                
-                <input required type="email" name="email" placeholder="Enter Email" class="input" id="email"
+                <input required type="email" name="email" placeholder="Enter Email" class="input p-2" id="email"
                 aria-invalid={$errors.email ? 'true' : undefined}
                 bind:value={$form.email}
                 {...$constraints.email}/>
+                
             </label>
             {#if $errors.email}<span class="invalid">{$errors.email}</span>{/if}
             <label class="label">
                 
-                <input required type="password" name="password" placeholder="Enter Password" class="input" id="password"
+                <input required type="password" name="password" placeholder="Enter Password" class="input p-2" id="password"
                 aria-invalid={$errors.password ? 'true' : undefined}
                 bind:value={$form.password}
                 {...$constraints.password}/>
 
             </label>
             {#if $errors.password}<span class="invalid">{$errors.password}</span>{/if}
-            <div class="flex justify-between flex-wrap">
-                <p class="text-sm unstyled py-2 text-slate-500">
+            <div class="flex justify-between flex-col">
+                <p class="text-sm py-2 text-slate-500">
                     Don't have an account? <a href="/register">Register</a>
                 </p>
                 <button class="btn variant-filled-primary">Login</button>
