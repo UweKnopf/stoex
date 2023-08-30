4f4e67ed46cbc7627cdd018d4c42b6c5e5358350
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-08-30 14:33:34 +0200

    Basic margins

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -19,37 +19,33 @@
         email: 'rconnechieb@chicagotribune.com'
     },
 ]
-    import { DataHandler, Datatable, Th, ThFilter } from '@vincjo/datatables' 
+    import { DataHandler, Datatable } from '@vincjo/datatables' 
 
     const handler = new DataHandler(myData, { rowsPerPage: 10 })
     const rows = handler.getRows()
 
 </script>
-<div class="table-container">
-    <Datatable {handler}>
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <Th {handler} orderBy="first_name">First Name</Th>
-                    <Th {handler} orderBy="last_name">Last Name</Th>
-                    <Th {handler} orderBy="email">Email</Th>
-                </tr>
-                <tr>
-                    <ThFilter {handler} filterBy="first_name"/>
-                    <ThFilter {handler} filterBy="last_name" />
-                    <ThFilter {handler} filterBy="email"/>
-                </tr>
-            </thead>
-            <tbody>
-                {#each $rows as row}
+
+<div class="m-8"> 
+    <div class="table-container">
+        <Datatable {handler}>
+            <table class="table table-hover">
+                <thead>
                     <tr>
-                        <td>{row.first_name}</td>
-                        <td>{row.last_name}</td>
-                        <td>{row.email}</td>
+                        <th>First name</th>
+                        <th>Last name</th>
                     </tr>
-                {/each}
-            </tbody>
-        </table>
-    </Datatable>
+                </thead>
+                <tbody>
+                    {#each $rows as row}
+                        <tr>
+                            <td>{row.first_name}</td>
+                            <td>{row.last_name}</td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </Datatable>
+    </div>
 </div>
 
