18d715189af42b38fa8bd59b19fae3e0d86601a8
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-10-15 20:08:39 +0200

    Sub Component Init

---
+++ src/lib/payment/SubscriptionTable.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { loadStripe } from '@stripe/stripe-js'
+    import { Elements } from 'svelte-stripe'
+    import { onMount } from 'svelte'
+    import { PUBLIC_STRIPE_KEY } from '$env/static/public'
+  
+    /**
+	 * @type {import("@stripe/stripe-js").Stripe | null}
+	 */
+    let stripe = null
+  
+    onMount(async () => {
+      stripe = await loadStripe(PUBLIC_STRIPE_KEY)
+    })
+  </script>
+  
+  {#if stripe}
+  <Elements {stripe}>
+    <!-- this is where your Stripe components go -->
+  </Elements>
+  {/if}
