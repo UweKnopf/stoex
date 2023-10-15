3f39c0aa0c44fc6bcb22705441d84ee8f8cb4fb4
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-10-15 20:15:37 +0200

    Add premade pricing table

--- src/lib/payment/SubscriptionTable.svelte
+++ src/lib/payment/SubscriptionTable.svelte
@@ -1,4 +1,4 @@
-<script>
+<script async src="https://js.stripe.com/v3/pricing-table.js">
     import { loadStripe } from '@stripe/stripe-js'
     import { Elements } from 'svelte-stripe'
     import { onMount } from 'svelte'
@@ -12,10 +12,16 @@
     onMount(async () => {
       stripe = await loadStripe(PUBLIC_STRIPE_KEY)
     })
+
+    
   </script>
+  
   
   {#if stripe}
   <Elements {stripe}>
+    <stripe-pricing-table pricing-table-id="prctbl_1O1Y8nKZzI6mW8k9FRRHAvZB"
+    publishable-key="pk_test_51O1UygKZzI6mW8k9QLe9eTx9yW3J14m7YGtKiI5asMNn9zuegVQPHUQeJDRuZdCIzfzHxRCqNqOlgtUxrPiDfmJ600vCBEscBO">
+    </stripe-pricing-table>
     <!-- this is where your Stripe components go -->
   </Elements>
   {/if}
