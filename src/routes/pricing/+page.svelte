aa9f982ea7ef811b01dc1aefe9580e0872c1eebd
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2023-11-09 13:21:20 +0100

    Payment link Working Checkout

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<a href="https://buy.stripe.com/test_3csdR6eb91AEfRefYY"> Buy Year Plan</a>
