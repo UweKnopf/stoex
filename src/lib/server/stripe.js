7e165fc3b46c4d34e361b01a54c9e6265c40ca9b
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-03-13 19:37:05 +0100

    Revert "Merge branch 'Stripe-2'"
    This reverts commit d4d3f4d0146ba473953dc50b9f67cc3773c8e573, reversing
    changes made to ca8542098a34d03f992faadf18b18addca3186e3.

--- src/lib/server/stripe.js
+++
@@ -1,9 +0,0 @@
-import Stripe from 'stripe'
-import { env } from '$env/dynamic/private'
-
-// export the stripe instance
-// @ts-ignore
-export const stripe = new Stripe(env.SECRET_STRIPE_KEY, {
-    // pin the api version
-    apiVersion: '2023-10-16'
-})
